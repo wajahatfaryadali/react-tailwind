4b003410160bc373ef40819132d9067e22b611b1
Wajahat Ali <wajahatfaryadali@gmail.com>
2023-08-27 21:15:40 +0500

    skills set

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -14,45 +14,41 @@ const HeroSection = () => {
             <div className=' max-w-[800px] mt-[-96px] w-full h-screen flex justify-center flex-col mx-auto text-center'>
                 <p className='text-[#00df9a] md:4xl sm:text-2xl text-sm font-bold'>Wajahat Faryad Ali</p>
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-3 sm:py-2 py-1'>Software Engineer</h1>
-                <div className='flex justify-center text-2xl'>
-                    <p>Skill set includes{"- "}</p>
+                <div className='flex justify-center text-xl'>
+                    <p>Skill set includes {" - "} </p>
                     <TypeAnimation
                         sequence={[
-                            'Front End Developer',
+                            ' UI Developer',
                             1000,
-                            'UI Developer',
+                            ' HTML',
                             1000,
-                            'Front End Programmer',
+                            ' CSS',
                             1000,
-                            'HTML',
+                            ' Javascript',
                             1000,
-                            'CSS',
+                            ' Jquery',
                             1000,
-                            'Javascript',
+                            ' React Js',
                             1000,
-                            'Jquery',
+                            ' React Redux',
                             1000,
-                            'DOM Manipulation',
+                            ' Core Redux',
                             1000,
-                            'React Js',
+                            ' Redux tool-kit',
                             1000,
-                            'React Redux',
+                            ' Redux Saga',
                             1000,
-                            'Redux (core/tool-kit)',
+                            ' React Routers',
                             1000,
-                            'Redux Saga',
+                            ' Material UI',
                             1000,
-                            'React Routers',
+                            ' Semantic UI',
                             1000,
-                            'Material UI',
+                            ' BootStrap',
                             1000,
-                            'Semantic UI',
+                            ' Tailwind',
                             1000,
-                            'BootStrap',
-                            1000,
-                            'Tailwind',
-                            1000,
-                            'Git',
+                            ' Git',
                             1000,
                         ]}
                         wrapper="span"
@@ -61,11 +57,10 @@ const HeroSection = () => {
                         repeat={Infinity}
                     />
                 </div>
-                <p className='md:text-2xl text-xl italic font-bold text-[#00df9a] md:py-2 py-1 mb-2'>
-
+                <p className='md:text-2xl text-xl italic font-bold text-[#00df9a] md:py-2 py-1 mb-2 px-2 md:px-0'>
                     This showcases my proficiency in creating a fully mobile-first, responsive UI design.
                 </p>
-                <p className='md:text-2xl text-xl font-bold text-gray-600 md:py-2 py-1 mb-2'>
+                <p className='md:text-2xl text-xl font-bold text-gray-600 md:py-2 py-1 mb-2 px-2 md:px-0'>
                     However, it does not include functionality to demonstrate my skill in that aspect.
                 </p>
                 <button className='bg-[#00df9a] w-[200px] text-md mx-auto rounded-md py-3' onClick={() => handleClick("Get Started")}>Get Started</button>
